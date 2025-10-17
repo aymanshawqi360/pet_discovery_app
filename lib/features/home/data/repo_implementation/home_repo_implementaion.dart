8d047d68c99867ebd0aa1538c1e2474d57261d1b
aymanshawqi360 <ayman22ahmadan2017@gmail.com>
2025-10-17 22:53:45 +0300

    added home_repo_implementaion to home folder

---
+++ lib/features/home/data/repo_implementation/home_repo_implementaion.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:pet_discovery_app/core/error/api_error_handler.dart';
+import 'package:pet_discovery_app/core/error/api_resulte.dart';
+import 'package:pet_discovery_app/features/home/data/api/home_api_service.dart';
+import 'package:pet_discovery_app/features/home/data/model/home_breed_molde.dart';
+import 'package:pet_discovery_app/features/home/domain/repo/home_repo.dart';
+
+class HomeRepoimplementaion implements HomeRepo {
+  final HomeApiService homeApiService;
+
+  HomeRepoimplementaion({required this.homeApiService});
+
+  @override
+  Future<ApiResulte<List<Breed>>> getBreeds({
+    required int limit,
+    required int page,
+  }) async {
+    try {
+      final response = await homeApiService.getBreeds();
+      return ApiResulte.success(response);
+    } catch (e) {
+      return ApiResulte.failure(ApiErrorHandler.apiHandler(e));
+    }
+  }
+}
