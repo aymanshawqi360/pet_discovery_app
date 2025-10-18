01ac1cfcb49bfdde8ae0e2488adaa1382b1f7be2
aymanshawqi360 <ayman22ahmadan2017@gmail.com>
2025-10-18 21:53:10 +0300

    Update app structure and config: - Modified app routing app_route.dart & routes.dart - Updated home screen UI and logic - Refactored pet_card and search view widgets - Updated main.dart & pet_discovery_app - Updated pubspec.yaml & pubspec.lock for dependency changes

--- lib/features/home/presentation/screen/home_screen.dart
+++ lib/features/home/presentation/screen/home_screen.dart
@@ -1,27 +1,12 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pet_discovery_app/core/di/dependency_injection.dart';
-import 'package:pet_discovery_app/core/routing/routes.dart';
-import 'package:pet_discovery_app/core/widgets/app_text_form_field.dart';
 import 'package:pet_discovery_app/features/home/presentation/cubit/home_cubit.dart';
 import 'package:pet_discovery_app/features/home/presentation/screen/search_screen.dart';
 import 'package:pet_discovery_app/features/home/presentation/widget/breeds/pet_card.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    context.read<HomeCubit>().getBreedState();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,34 +40,16 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             const SizedBox(height: 16),
 
-            // Center(
-            //   child: Padding(
-            //     padding: const EdgeInsets.all(16.0),
-            //     child: AppTextFormField(
-            //       filled: true,
-
-            //       suffixIcon: Icon(Icons.tune, color: Colors.black),
-            //       prefixIcon: GestureDetector(
-            //         onTap: () => Navigator.pushNamed(context, Routes.search),
-            //         child: Icon(Icons.search, color: Colors.black),
-            //       ),
-            //     ),
-            //   ),
-            // ),
             Expanded(
               child: BlocBuilder<HomeCubit, HomeState>(
                 builder: (context, state) {
                   if (state is HomeSuccess) {
-                    log(state.homeBreed[0].referenceImageId.toString());
                     return ListView.separated(
                       separatorBuilder: (context, index) =>
                           SizedBox(height: 15),
                       itemCount: state.homeBreed.length,
                       itemBuilder: (context, index) {
                         return PetCard(breed: state.homeBreed[index]);
-                        //  Image.network(
-                        //   "https://cdn2.thecatapi.com/images/${state.homeBreed[index].referenceImageId}.jpg",
-                        // );
                       },
                     );
                   } else if (state is HomeFailure) {
--- lib/pet_discovery_app.dart
+++ lib/pet_discovery_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pet_discovery_app/core/routing/app_route.dart';
+import 'package:pet_discovery_app/core/routing/routes.dart';
 
 class PetDiscoveryApp extends StatelessWidget {
   final AppRouting appRouting;
@@ -9,6 +10,7 @@ class PetDiscoveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute: Routes.navigatorExample,
       onGenerateRoute: appRouting.onGenerateRoute,
     );
   }
